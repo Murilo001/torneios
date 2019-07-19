05e5d05166b0dd2e13145e3396c518e3a82c449d
Augusto Moura <augusto.borgesm@gmail.com>
2019-07-18 21:14:02 -0300

    transforma o projeto em só o front

--- model/db.js
+++
@@ -1,6 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path')
-const dbPath = path.join(__dirname, './torneio.db');
-console.log(dbPath)
-
-module.exports = new sqlite3.Database(dbPath)
